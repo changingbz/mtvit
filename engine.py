1747b1391480c1a04ba5fb03671a8484b213d7f1
bianzhe21 <bianzhe1999@163.com>
2023-05-06 15:27:16 +0800

    Changes to be committed: 	new file:   engine.py 	new file:   helpers.py 	new file:   losses.py

---
+++ engine.py
@@ -0,0 +1,218 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+# ------------------------------------------
+# Modification:
+# Added code for adjusting keep rate and visualization -- Youwei Liang
+"""
+Train and eval functions used in main.py
+"""
+import math
+import random
+import sys
+from typing import Iterable, Optional
+
+import numpy as np
+import torch
+
+from timm.data import Mixup
+from timm.utils import accuracy, ModelEma
+
+from losses import DistillationLoss
+import utils
+
+from helpers import adjust_keep_rate
+from visualize_mask import get_real_idx, mask, save_img_batch, make_visualization, getCluster
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+import kornia as k
+import torch.nn as nn
+from timm.data.random_erasing import RandomErasing
+
+class Transforms(nn.Module):
+    def __init__(self):
+        super(Transforms, self).__init__()
+        self.aug_list = k.augmentation.container.ImageSequential(
+            k.augmentation.ColorJitter(p=0.3),
+            # k.geometry.transform.rescale(interpolation='bicubic'),
+            k.augmentation.RandomErasing(p=0.25)
+        )
+
+    def forward(self, img):
+
+        img = self.aug_list(img)
+
+        return img
+
+def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None,
+                    writer=None,
+                    set_training_mode=True,
+                    args=None):
+    model.train(set_training_mode)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 200
+    log_interval = 100
+    it = epoch * len(data_loader)
+    ITERS_PER_EPOCH = len(data_loader)
+    idx = 0
+    base_rate = args.base_keep_rate
+    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+        samples = samples.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        keep_rate = adjust_keep_rate(it, epoch, warmup_epochs=args.shrink_start_epoch,
+                                         total_epochs=args.shrink_start_epoch + args.shrink_epochs,
+                                         ITERS_PER_EPOCH=ITERS_PER_EPOCH, base_keep_rate=base_rate)
+
+        if mixup_fn is not None:
+            samples, targets = mixup_fn(samples, targets)
+        with torch.cuda.amp.autocast():
+            outputs = model(samples, keep_rate)
+            loss = criterion(samples, outputs, targets)
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+        # this attribute is added by timm on one optimizer (adahessian)
+        is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=is_second_order)
+        torch.cuda.synchronize()
+        if model_ema is not None:
+            model_ema.update(model)
+        metric_logger.update(loss=loss_value)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        
+        if torch.distributed.get_rank() == 0 and it % log_interval == 0:
+            writer.add_scalar('loss', loss_value, it)
+            writer.add_scalar('lr', optimizer.param_groups[0]["lr"], it)
+            writer.add_scalar('keep_rate', keep_rate, it)
+        it += 1
+        idx += 1
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}, keep_rate
+
+
+@torch.no_grad()
+def evaluate(data_loader, model, device, keep_rate=None):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    for images, target in metric_logger.log_every(data_loader, 10, header):
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+
+        # compute output
+        with torch.cuda.amp.autocast():
+            output = model(images, keep_rate)
+            loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def get_acc(data_loader, model, device, keep_rate=None, tokens=None):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    for images, target in metric_logger.log_every(data_loader, 10, header):
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+
+        # compute output
+        with torch.cuda.amp.autocast():
+            output = model(images, keep_rate, tokens)
+            loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return metric_logger.acc1.global_avg
+
+
+@torch.no_grad()
+def visualize_mask(data_loader, model, device, output_dir, n_visualization, keep_rate=None):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Visualize:'
+   # rank = torch.distributed.get_rank()
+    rank = 1
+   # world_size = torch.distributed.get_world_size()
+    world_size = 1
+    mean = torch.tensor(IMAGENET_DEFAULT_MEAN, device=device).reshape(3, 1, 1)
+    std = torch.tensor(IMAGENET_DEFAULT_STD, device=device).reshape(3, 1, 1)
+
+    # switch to evaluation mode
+    model.eval()
+
+    ii = 0
+    for images, target in metric_logger.log_every(data_loader, 10, header):
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+        B = images.size(0)
+
+        with torch.cuda.amp.autocast():
+            output, idx, nidxs, coss = model(images, keep_rate, get_idx=True)
+            loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        images = images * std + mean
+        idxs = get_real_idx(idx)
+        alls = getCluster(idxs, nidxs, coss)
+        for jj, idx in enumerate(alls):
+            masked_img = make_visualization(images, alls[jj])
+            save_img_batch(masked_img, output_dir, file_name='img_{}' + f'_l{jj}.jpg', start_idx=world_size * B * ii + rank * B)
+        save_img_batch(images, output_dir, file_name='img_{}_a.jpg', start_idx=world_size * B * ii + rank * B)
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        metric_logger.synchronize_between_processes()
+        ii += 1
+        if world_size * B * ii >= n_visualization:
+            break
+
+    metric_logger.synchronize_between_processes()
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
---
+++ helpers.py
@@ -0,0 +1,87 @@
+import math
+import time
+import torch
+from torchprofile import profile_macs
+import torch.nn.functional as F
+
+
+def adjust_keep_rate(iters, epoch, warmup_epochs, total_epochs,
+                       ITERS_PER_EPOCH, base_keep_rate=0.5, max_keep_rate=1):
+    if epoch < warmup_epochs:
+        return 1
+    if epoch >= total_epochs:
+        return base_keep_rate
+    total_iters = ITERS_PER_EPOCH * (total_epochs - warmup_epochs)
+    iters = iters - ITERS_PER_EPOCH * warmup_epochs
+    keep_rate = base_keep_rate + (max_keep_rate - base_keep_rate) \
+        * (math.cos(iters / total_iters * math.pi) + 1) * 0.5
+
+    return keep_rate
+
+
+def speed_test(model, ntest=100, batchsize=64, x=None, **kwargs):
+    if x is None:
+        img_size = model.img_size
+        x = torch.rand(batchsize, 3, *img_size).cuda()
+    else:
+        batchsize = x.shape[0]
+    model.eval()
+
+    start = time.time()
+    for i in range(ntest):
+        model(x, **kwargs)
+    torch.cuda.synchronize()
+    end = time.time()
+
+    elapse = end - start
+    speed = batchsize * ntest / elapse
+    # speed = torch.tensor(speed, device=x.device)
+    # torch.distributed.broadcast(speed, src=0, async_op=False)
+    # speed = speed.item()
+    return speed
+
+
+def complement_idx(idx, dim):
+    """
+    Compute the complement: set(range(dim)) - set(idx).
+    idx is a multi-dimensional tensor, find the complement for its trailing dimension,
+    all other dimension is considered batched.
+    Args:
+        idx: input index, shape: [N, *, K]
+        dim: the max index for complement
+    """
+    a = torch.arange(dim, device=idx.device)
+    ndim = idx.ndim
+    dims = idx.shape
+    n_idx = dims[-1]
+    dims = dims[:-1] + (-1, )
+    for i in range(1, ndim):
+        a = a.unsqueeze(0)
+    a = a.expand(*dims)
+    masked = torch.scatter(a, -1, idx, 0)
+    compl, _ = torch.sort(masked, dim=-1, descending=False)
+    compl = compl.permute(-1, *tuple(range(ndim - 1)))
+    compl = compl[n_idx:].permute(*(tuple(range(1, ndim)) + (0,)))
+    return compl
+
+def upsample(x, size=7):
+    feature_temp = x[:, 1:]
+    B, new_HW, C = feature_temp.shape
+    feature_temp = feature_temp.transpose(1, 2).contiguous().reshape(B, C, size, size)
+    feature_temp = torch.nn.functional.interpolate(feature_temp, (14, 14), mode='nearest')
+    feature_temp = feature_temp.view(B, C, -1).transpose(1, 2)
+    feature_temp = torch.cat([x[:, 0:1], feature_temp], dim=1)
+    return feature_temp
+
+
+def downsample(x, dim):
+    weight = torch.ones(dim, 1, 2, 2).cuda() * 0.25
+    bias = torch.zeros(dim).cuda()
+    feature_temp = x[:, 1:]
+    B, new_HW, C = feature_temp.shape
+    feature_temp = feature_temp.transpose(1, 2).contiguous().reshape(B, C, 14, 14)
+    feature_temp = F.conv2d(feature_temp, weight, bias, stride=2, padding=0, groups=dim)
+    feature_temp = feature_temp.view(B, C, -1).transpose(1, 2)
+    feature_temp = torch.cat([x[:, 0:1], feature_temp], dim=1)
+
+    return feature_temp
